bf954c8fa77e4b7d4e2de9c7e4354f5b173d2271
Fierynight <84808424+fieryknight2@users.noreply.github.com>
2024-06-16 17:00:04 -0500

    Simplified asserts

--- include/simplelogger.hpp
+++ include/simplelogger.hpp
@@ -68,11 +68,15 @@ private:
 
 /* Do not log assertions or debug info on release builds*/
 #define SL_ASSERT_FALSE(condition, message)                                                                            \
+    if (condition)                                                                                                     \
+    {                                                                                                                  \
+        throw slog::LogException(message);                                                                             \
+    }
+#define SL_ASSERT_TRUE(condition, message)                                                                             \
     if (!(condition))                                                                                                  \
     {                                                                                                                  \
         throw slog::LogException(message);                                                                             \
     }
-#define SL_ASSERT_TRUE(condition, message) SL_ASSERT_FALSE(!(condition), message)
 #define SL_ASSERT(condition, message) SL_ASSERT_TRUE(condition, message)
 
 #else // NDEBUG
