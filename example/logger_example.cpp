6f18ad6ec026aa204b0ea07f2f444f4c68b5fae0
Fierynight <84808424+fieryknight2@users.noreply.github.com>
2024-06-16 14:10:11 -0500

    Renamed SF to SL

--- example/logger_example.cpp
+++ example/logger_example.cpp
@@ -15,51 +15,54 @@ int main(const int argc, char *argv[])
     slog::SimpleLogger::GlobalLogger()->setMinLogLevel(slog::LogLevel::DEBUG);
     std::cout << "Log level: " << slog::getLogName(slog::SimpleLogger::GlobalLogger()->getMinLogLevel()) << std::endl;
 
-    SF_LOG_VERSION_INFO();
+    SL_LOG_VERSION_INFO();
 
-    SF_LOG_WARNING("Ran using " + std::string(argv[0]));
+    SL_LOG_WARNING("Ran using " + std::string(argv[0]));
 
-    SF_LOG_DEBUG("This is a debug message");
-    SF_LOG_DEBUG("This is a debug message");
-    SF_LOG_DEBUG("This is a debug message");
-    SF_LOG_DEBUG("This is a debug message");
-    SF_LOG_INFO("This is an info message");
-    SF_LOG_WARNING("This is a warning message");
-    SF_LOG_ERROR("This is an error message");
-    SF_LOG_FATAL("This is a fatal message");
+    SL_LOG_DEBUG("This is a debug message");
+    SL_LOG_DEBUG("This is a debug message");
+    SL_LOG_DEBUG("This is a debug message");
+    SL_LOG_DEBUG("This is a debug message");
+    SL_LOG_INFO("This is an info message");
+    SL_LOG_WARNING("This is a warning message");
+    SL_LOG_ERROR("This is an error message");
+    SL_LOG_FATAL("This is a fatal message");
 
     for (int i = 0; i < argc; i++)
     {
-        SF_LOG_DEBUG("Argument: " + std::to_string(i) + " is " + std::string(argv[i]));
+        SL_LOG_DEBUG("Argument: " + std::to_string(i) + " is " + std::string(argv[i]));
     }
 
-    SF_LOG_INFO("Opening file example.log for logging");
+    SL_LOG_INFO("Opening file example.log for logging");
     slog::SimpleLogger::GlobalLogger()->addLogger(std::make_shared<slog::FileLogger>("example.log"));
 
-    SF_LOG_DEBUG("This is a debug message");
+    SL_LOG_DEBUG("This is a debug message");
     for (int i = 0; i < 100; i++)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(250));
-        SF_LOG_DEBUG("This is a debug message");
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        SL_LOG_DEBUG("This is a debug message");
     }
 
-    SF_LOG_INFO("This is an info message");
-    SF_LOG_WARNING("This is a warning message");
-    SF_LOG_ERROR("This is an error message");
-    SF_LOG_FATAL("This is a fatal message");
+    SL_LOG_INFO("Enabling color");
+    std::dynamic_pointer_cast<slog::ConsoleLogger>(slog::SimpleLogger::GlobalLogger()->getLogger(0))->enableColor(true);
+
+    SL_LOG_INFO("This is an info message");
+    SL_LOG_WARNING("This is a warning message");
+    SL_LOG_ERROR("This is an error message");
+    SL_LOG_FATAL("This is a fatal message");
 
     slog::SimpleLogger::GlobalLogger()->exception(slog::LogException("This is an exception"));
 
     try
     {
-        SF_ASSERT(false, "This will fail.");
+        SL_ASSERT(false, "This will fail.");
     }
     catch (const slog::LogException &exception)
     {
         slog::SimpleLogger::GlobalLogger()->exception(exception);
     }
 
-    SF_ASSERT(false, "The end.");
+    SL_ASSERT(false, "The end.");
 
     return 0;
 }
--- include/simplelogger.hpp
+++ include/simplelogger.hpp
@@ -12,7 +12,7 @@
 
 #include "loggerloc.hpp"
 
-#define SF_LOG_VERSION_INFO() SF_LOG_INFO("--- Using SimpleLogger " + slog::SimpleLoggerVersion + " ---")
+#define SL_LOG_VERSION_INFO() SL_LOG_INFO("--- Using SimpleLogger " + slog::SimpleLoggerVersion + " ---")
 
 namespace slog
 {
@@ -58,7 +58,7 @@ private:
 #ifndef NDEBUG
 
 /* Do not log assertions or debug info on release builds*/
-#define SF_ASSERT(condition, message)                                                                                  \
+#define SL_ASSERT(condition, message)                                                                                  \
     if (!(condition))                                                                                                  \
     {                                                                                                                  \
         throw slog::LogException(message);                                                                             \
@@ -66,13 +66,13 @@ private:
 
 #else // NDEBUG
 
-#define SF_ASSERT(condition, message)
+#define SL_ASSERT(condition, message)
 
 #endif // NDEBUG
 
-#define SF_LOG_EXCEPTION(exception) slog::SimpleLogger::GlobalLogger()->exception(exception)
+#define SL_LOG_EXCEPTION(exception) slog::SimpleLogger::GlobalLogger()->exception(exception)
 
-#ifdef SF_MIN_LOG_LEVEL
+#ifdef SL_MIN_LOG_LEVEL
 
 /* Logging macros
  *
@@ -84,53 +84,53 @@ private:
  *
  * The number will also log every level below it,ex: 2 would log warning, error, and fatal
  */
-#if SF_MIN_LOG_LEVEL > 3
+#if SL_MIN_LOG_LEVEL > 3
 
 #ifndef NDEBUG
 
-#define SF_LOG_DEBUG(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::DEBUG)
+#define SL_LOG_DEBUG(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::DEBUG)
 
 #else
 
-#define SF_LOG_DEBUG(message)
+#define SL_LOG_DEBUG(message)
 
 #endif // NDEBUG
 
-#else // SF_MIN_LOG_LEVEL == 0
-#define SF_LOG_DEBUG(message)
+#else // SL_MIN_LOG_LEVEL == 0
+#define SL_LOG_DEBUG(message)
 #endif // SF_MIN_LOG_LEVEL == 0
 
-#if SF_MIN_LOG_LEVEL > 2
+#if SL_MIN_LOG_LEVEL > 2
 #define SF_LOG_INFO(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::INFO)
 #else
 #define SF_LOG_INFO(message)
 #endif // SF_MIN_LOG_LEVEL > 0
 
-#if SF_MIN_LOG_LEVEL > 1
-#define SF_LOG_WARNING(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::WARNING)
+#if SL_MIN_LOG_LEVEL > 1
+#define SL_LOG_WARNING(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::WARNING)
 #else
-#define SF_LOG_WARNING(message)
+#define SL_LOG_WARNING(message)
 #endif // SF_MIN_LOG_LEVEL > 1
 
-#if SF_MIN_LOG_LEVEL > 0
-#define SF_LOG_ERROR(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::ERROR)
+#if SL_MIN_LOG_LEVEL > 0
+#define SL_LOG_ERROR(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::ERROR)
 #else
-#define SF_LOG_ERROR(message)
+#define SL_LOG_ERROR(message)
 #endif // SF_MIN_LOG_LEVEL > 2
 
-#if SF_MIN_LOG_LEVEL > -1
-#define SF_LOG_FATAL(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::FATAL)
+#if SL_MIN_LOG_LEVEL > -1
+#define Sl_LOG_FATAL(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::FATAL)
 #else
-#define SF_LOG_FATAL(message)
-#endif // SF_MIN_LOG_LEVEL > 3
+#define SL_LOG_FATAL(message)
+#endif // SL_MIN_LOG_LEVEL > 3
 
-#else // SF_MIN_LOG_LEVEL
+#else // SL_MIN_LOG_LEVEL
 
 // Log level not set, so default to everything
-#define SF_LOG_DEBUG(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::DEBUG)
-#define SF_LOG_INFO(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::INFO)
-#define SF_LOG_WARNING(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::WARNING)
-#define SF_LOG_ERROR(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::ERROR)
-#define SF_LOG_FATAL(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::FATAL)
+#define SL_LOG_DEBUG(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::DEBUG)
+#define SL_LOG_INFO(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::INFO)
+#define SL_LOG_WARNING(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::WARNING)
+#define SL_LOG_ERROR(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::ERROR)
+#define SL_LOG_FATAL(message) slog::SimpleLogger::GlobalLogger()->log(message, slog::LogLevel::FATAL)
 
-#endif // SF_MIN_LOG_LEVEL
+#endif // SL_MIN_LOG_LEVEL
